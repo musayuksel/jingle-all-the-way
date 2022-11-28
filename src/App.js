5b0d04e7d5b4fd257f640c3aad83875e7a8070ec
Musa <79163385+musayuksel@users.noreply.github.com>
2022-11-28 20:12:36 +0000

    Add input and functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
-
+import { santaCurrentPosition } from './utils/santaCurrentPosition';
+import routes from './utils/routes.json';
 function App() {
+  const [value, setValue] = useState(
+    `${new Date(routes[0].time).toISOString().split('T')[0]}T20:59`
+  );
+
+  const arraivalTimeMessage = santaCurrentPosition(value);
+
   return (
-    <div className="App">
-     Jingle Bells
+    <div className='App'>
+      <label htmlFor='santaCurrentTime'>Choose a time </label>
+
+      <input
+        type='datetime-local'
+        id='santaCurrentTime'
+        name='santaCurrentTime'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        min={`${new Date(routes[0].time).toISOString().split('T')[0]}T00:00`}
+        max={`${
+          new Date(routes[routes.length - 1].time).toISOString().split('T')[0]
+        }T23:59`}
+      />
+      <h2>{arraivalTimeMessage}</h2>
     </div>
   );
 }
